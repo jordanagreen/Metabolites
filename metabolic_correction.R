2415095bd7db7617c09972d8d330ca200cc32ee1
Jordan Green <thegreenjordan@gmail.com>
2016-04-04 14:05:05 -0400

    Remove unnecessary method parameters

--- metabolic_correction.R
+++ metabolic_correction.R
@@ -4,19 +4,19 @@
 #'
 #' @param samples a vector of the samples to be corrected
 #' @param qcs a vector of the QCs to be used in correction
-#' @param betweens a vector of which QCs the sample at position i is between; 
-#'        i.e. a sample i between QCs 1-2 will have the value 1 in this vector
-#' @param qcpositions a vector of the positions of each QC in the overall data
-#' @param sample_positions a vector of the positions of each sample in the overall data
+#' @param qc_positions a vector of the positions of each QC in the overall data;
+#'        it is assumed that the last QC is the last value in the overall data.
 #' @param w the number of adjacent QCs to use in creating the regression line for each sample in the Virtual-QC method
 #'
 #' @return A vector of normalized intensity values
 #' @export
 #'
 #' @examples
-normalize <- function(samples, qcs, betweens, qcpositions, sample_positions, w){
-  normalized <- mapply(sys_correct, samples, betweens, sample_positions, 
-                       MoreArgs=list(qcs=qcs, qcpositions=qcpositions, sample_positions, w=w))
+normalize <- function(samples, qcs, qc_positions, w){
+  n <- max(qc_positions)
+  sample_positions <- setdiff(1:n, qc_positions)
+  normalized <- mapply(sys_correct, samples, sample_positions, 
+                       MoreArgs=list(qcs=qcs, qc_positions=qc_positions, w=w))
   return(normalized)
 }
 
@@ -24,22 +24,23 @@ normalize <- function(samples, qcs, betweens, qcpositions, sample_positions, w){
 #' 
 #' Systematically corrects the intensity value of a metabolite sample using the Virtual-QC method
 #'
-#' @param sample the sample to be corrected
-#' @param between the QC number which this sample comes after in the overall ordering
-#' @param i the sample's position in the overall ordering
+#' @param sample the sample intensity value to be corrected
+#' @param i the sample's position in the overall ordering, including QCs
 #' @param qcs a vector of the QCs to be used in correction
-#' @param qcpositions a vector of the positions of each QC in the overall data
-#' @param sample_positions a vector of the positions of each sample in the overall data
+#' @param qc_positions a vector of the positions of each QC in the overall data
 #' @param w the number of adjacent QCs to use in creating the regression line for each sample in the Virtual-QC method
 #'
 #' @return A normalized intensity value
 #' @export
 #'
 #' @examples
-sys_correct <- function(sample, between, i, qcs, qcpositions, sample_positions, w){
-  from <- max(1, between - (w %/% 2) + 1)
-  to <- min(between + (w %/% 2), length(qcs))
-  cf <- get_correction_factor(qcs, qcpositions, from, to, i)
+sys_correct <- function(sample, i, qcs, qc_positions, w){
+  #calculate the index of the QC this sample comes after
+  after_qc <- 1
+  while(qc_positions[after_qc+1] < i) after_qc = after_qc + 1
+  from <- max(1, after_qc - (w %/% 2) + 1)
+  to <- min(after_qc + (w %/% 2), length(qcs))
+  cf <- get_correction_factor(qcs, qc_positions, from, to, i)
   corrected <- sample / cf
   return(corrected)
 }
@@ -49,7 +50,7 @@ sys_correct <- function(sample, between, i, qcs, qcpositions, sample_positions, 
 #' Calculates a correction factor to use for correcting an intensity value in the Virtual-QC method
 #'
 #' @param qcs a vector of the QCs to be used in correction
-#' @param qcpositions a vector of the positions of each QC in the overall data
+#' @param qc_positions a vector of the positions of each QC in the overall data
 #' @param from the smallest QC index to be considered in the regression line
 #' @param to the largest QC index to be considered in the regression line
 #' @param i the sample's position in the overall ordering
@@ -58,18 +59,12 @@ sys_correct <- function(sample, between, i, qcs, qcpositions, sample_positions, 
 #' @export
 #'
 #' @examples
-get_correction_factor <- function(qcs, qcpositions, from, to, i){
-  regression <- lm(qcs[from:to] ~ qcpositions[from:to])
+get_correction_factor <- function(qcs, qc_positions, from, to, i){
+  regression <- lm(qcs[from:to] ~ qc_positions[from:to])
   a <- regression$coefficients[[2]]
   b <- regression$coefficients[[1]]
   return(a*(i+1) + b)
 }
-
-# correct_qcs <- function(combined_data, qcpositions, w){
-#   corrected = sapply(1:length(qcpositions), correct_qc, combined_data, qcpositions, w)
-#   # print(corrected)
-#   return(corrected)
-# }
 
 #' Corrects QCs using regression lines
 #' 
@@ -77,30 +72,33 @@ get_correction_factor <- function(qcs, qcpositions, from, to, i){
 #'   and adjusting them to lie on those lines
 #'
 #' @param qcs the vector of QCs to be corrected
-#' @param qcpositions a vector of the positions of each QC in the overall data
+#' @param qc_positions a vector of the positions of each QC in the overall data
 #' @param w the number of adjacent QCs to use in the regression line
 #'
 #' @return A vector of corrected QC intensity values
 #' @export
 #'
 #' @examples
-correct_qcs <- function(qcs, qcpositions, w){
-  corrected <- sapply(1:length(qcs), correct_qc, qcs, qcpositions, w)
+correct_qcs <- function(qcs, qc_positions, w){
+  corrected <- sapply(1:length(qcs), correct_qc, qcs, qc_positions, w)
   return(corrected)
 }
 
-#' Title
+#' Corrects a QC using a regression line
+#' 
+#' Corrects a QC intensity value by adjusting it to lie on a regression line
+#'   of w adjacent QCs
 #'
 #' @param i the index of the QC to be corrected
 #' @param qcs the vector of QCs to be corrected
-#' @param qcpositions a vector of the positions of each QC in the overall data
+#' @param qc_positions a vector of the positions of each QC in the overall data
 #' @param w the number of adjacent QCs to use in the regression line
 #'
 #' @return The corrected intensity value for QC i
 #' @export
 #'
 #' @examples
-correct_qc <- function(i, qcs, qcpositions, w){
+correct_qc <- function(i, qcs, qc_positions, w){
   
   #TODO: this logic is stupid, make it better
   # index of QCs in overall data
@@ -109,52 +107,52 @@ correct_qc <- function(i, qcs, qcpositions, w){
     to <- min(i+((w-1)/2), length(qcs))
     # if (i > w/2){
       # from_i <- i-((w-1)/2)
-      # to_i <- min(i+((w-1)/2), length(qcpositions))
+      # to_i <- min(i+((w-1)/2), length(qc_positions))
       
     # }
     # else {
       # from_i <- 1
-      # to_i <- min(i+((w-1)/2), length(qcpositions))
+      # to_i <- min(i+((w-1)/2), length(qc_positions))
     # }
     # from_i <- max(1, i-((w-1)/2))
-    # to_i <- min(i+((w-1)/2), length(qcpositions))
+    # to_i <- min(i+((w-1)/2), length(qc_positions))
   }
   else {
     if (i > (w/2)){
       from <- i-(w/2)
       to <- min(i+(w/2)-1, length(qcs))
       # from_i <- i-(w/2)
-      # to_i <- min(i+(w/2)-1, length(qcpositions))
+      # to_i <- min(i+(w/2)-1, length(qc_positions))
     }
     else {
       from <- 0
       to <- min(i+(w/2), length(qcs))
       # from_i <- 1
-      # to_i <- min(i+(w/2), length(qcpositions))
+      # to_i <- min(i+(w/2), length(qc_positions))
     }
   }
-  # from <- qcpositions[from_i]
-  # to <- qcpositions[to_i]
+  # from <- qc_positions[from_i]
+  # to <- qc_positions[to_i]
   # print(paste("i=",i))
   # print(paste("i from", from_i, "to", to_i))
   # print(paste("x from", from, "to", to))
   
   # fit <- lm(unlist(combined_data[from:to])~seq(from, to))
-  fit <- lm(qcs[from:to]~qcpositions[from:to])
+  fit <- lm(qcs[from:to]~qc_positions[from:to])
   a <- fit$coefficients[[2]]
   b <- fit$coefficients[[1]]
 
-  corrected <- a*(qcpositions[i]) + b
-  # plot(qcpositions, qcs)
+  corrected <- a*(qc_positions[i]) + b
+  # plot(qc_positions, qcs)
   # abline(fit)
-  # points(qcpositions[i], corrected, col="blue", pch=19)
-  # print(paste("x=",qcpositions[i]))
+  # points(qc_positions[i], corrected, col="blue", pch=19)
+  # print(paste("x=",qc_positions[i]))
   # print(fit)
   # print(data[from:to])
   # print(seq(from, to))
   # plot(data[from:to])
   # abline(fit)
-  # print(c(qcpositions[i], corrected))
+  # print(c(qc_positions[i], corrected))
   # points(list(x=positions[i], y=corrected), col="red", pch=19)
 
   return(corrected)
